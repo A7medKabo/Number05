829fe29477b0cf79f9ad9170e811140aa1de1c38
A7medKabo <46240483+A7medKabo@users.noreply.github.com>
2019-04-10 11:24:05 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "540585853135880242"; // ايدي السررفر
+var channel = "565457947891859466";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
+
+ 
+client.login(process.env.BOT_TOKEN);
